51691f398fc357b0879ec03914a6de0549749cae
julibnk <fernandezfernandezjulian@gmail.com>
2024-11-16 16:18:13 +0100

    delete unusued layouts

--- web/src/configs/navigation.config/index.tsx
+++ web/src/configs/navigation.config/index.tsx
@@ -1,25 +1,25 @@
-import type {NavigationTree} from '@/@types/navigation';
-import {IconDashboard, IconUser} from '@tabler/icons-react';
+import type { NavigationTree } from '@/@types/navigation';
+import { IconHome, IconUser } from '@tabler/icons-react';
 
 const navigationConfig: NavigationTree[] = [
   {
-    key: 'dashboard',
-    path: '/dashboard',
-    title: 'Dashboard',
+    key: 'mortages',
+    path: '/mortages',
+    title: 'Mortages',
     translateKey: '',
-    icon: IconDashboard,
+    icon: IconHome,
     authority: [],
-    subMenu: []
+    subMenu: [],
   },
-  {
-    key: 'users',
-    path: '/users',
-    title: 'Users',
-    translateKey: '',
-    icon: IconUser,
-    authority: [],
-    subMenu: []
-  },
+  //{
+  //  key: 'users',
+  //  path: '/users',
+  //  title: 'Users',
+  //  translateKey: '',
+  //  icon: IconUser,
+  //  authority: [],
+  //  subMenu: []
+  //},
 ];
 
 export default navigationConfig;
