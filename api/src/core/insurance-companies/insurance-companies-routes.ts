1075eeb7d7e59d3af9af39a942e844e66f1998a3
julibnk <fernandezfernandezjulian@gmail.com>
2024-11-16 15:39:39 +0100

    Single get request

--- api/src/core/insurance-companies/insurance-companies-routes.ts
+++ api/src/core/insurance-companies/insurance-companies-routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
 import { InsuranceCompanyResponseSchema } from './insurance-companies-schemas.js';
 
 export default async function (server: FastifyInstance) {
@@ -12,4 +13,34 @@ export default async function (server: FastifyInstance) {
       return insurance;
     },
   });
+  server.withTypeProvider<ZodTypeProvider>().route({
+    method: 'GET',
+    url: '/:id',
+    schema: {
+      params: z.object({ id: z.string().uuid() }),
+      response: {
+        200: InsuranceCompanyResponseSchema,
+        404: z.null(),
+      },
+    },
+    handler: async (req, res) => {
+      const insurance = await server.prisma.insuranceCompany.findUnique({
+        where: { id: req.params.id },
+      });
+      if (!insurance) {
+        res.status(404);
+        return;
+      }
+      return insurance;
+    },
+  });
+  //server.withTypeProvider<ZodTypeProvider>().route({
+  //  method: 'GET',
+  //  url: '/',
+  //  schema: { response: { 200: InsuranceCompanyResponseSchema.array() } },
+  //  handler: async () => {
+  //    const insurance = await server.prisma.insuranceCompany.findMany();
+  //    return insurance;
+  //  },
+  //});
 }
